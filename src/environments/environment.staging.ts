a2d4d7437262c5faabfe0f6ace0512ed7764680e
Gion-Andri Cantieni <gion-andri@gmx.ch>
2023-10-31 12:16:31 +0100

    feat: staging can now https

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,6 +3,6 @@ import { Environment } from "./ienvironment";
 export const environment: Environment = {
     production: false,
     name: 'staging',
-    apiBasePath: 'http://api-staging.chalender.ch/api/',
+    apiBasePath: 'https://api-staging.chalender.ch/api/',
     host: 'api-staging.chalender.ch'
 }
