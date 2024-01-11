ca9b36ad66c206ccddd77bf0face2ab4cb614031
Gion-Andri Cantieni <gion-andri@gmx.ch>
2024-01-11 20:45:02 +0100

    feat: create gate page for notices

--- src/app/user-area/pages/notices/notices.component.ts
+++ src/app/user-area/pages/notices/notices.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../shared/services/authentication.service';
 
 @Component({
-  selector: 'app-notices',
-  templateUrl: './notices.component.html',
-  styleUrls: ['./notices.component.scss']
+    selector: 'app-notices',
+    templateUrl: './notices.component.html',
+    styleUrls: ['./notices.component.scss']
 })
 export class NoticesComponent {
 
+    constructor(private router: Router, private authService: AuthenticationService) {
+    }
+
+    ngOnInit() {
+        if (this.authService.isLoggedIn()) {
+            this.createNotice();
+        }
+    }
+
+    login(): void {
+        this.router.navigate(['/user/login'], {queryParams: {redirectTo: '/user/notice-form'}});
+    }
+
+    createNotice(): void {
+        this.router.navigate(['/user/notice-form']);
+    }
 }
