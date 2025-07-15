90913993e31540bc49937f1ae081303dfbefd6e4
Gion-Andri Cantieni <gion-andri@gmx.ch>
2025-07-15 12:25:51 +0200

    chore: update server script

--- server.ts
+++ server.ts
@@ -1,36 +1,29 @@
+import 'zone.js/node';
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr/node';
+import { REQUEST, RESPONSE } from './src/express.tokens';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
-import { REQUEST, RESPONSE } from './src/express.tokens';
 import { renderApplication } from '@angular/platform-server';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './src/app/app.component';
 import { serverConfig } from './src/app/app.config.server';
+import { readFileSync } from 'node:fs';
 
-// The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
     const server = express();
     const serverDistFolder = dirname(fileURLToPath(import.meta.url));
     const browserDistFolder = resolve(serverDistFolder, '../browser');
-    const indexHtml = join(serverDistFolder, 'index.server.html');
 
-    const commonEngine = new CommonEngine();
+    const indexHtml = readFileSync(join(serverDistFolder, 'index.server.html'), 'utf-8');
 
-    server.set('view engine', 'html');
-    server.set('views', browserDistFolder);
+    server.use(
+        express.static(browserDistFolder, {
+            maxAge: '1y',
+        }),
+    );
 
-    // Example Express Rest API endpoints
-    // server.get('/api/**', (req, res) => { });
-    // Serve static files from /browser
-    server.get('**', express.static(browserDistFolder, {
-        maxAge: '1y',
-        index: 'index.html',
-    }));
-
-    // All regular routes use the Angular engine
-    server.get('**', async (req, res, next) => {
+    server.get(/^\/.*$/, async (req, res, next) => {
         try {
             const html = await renderApplication(
                 () => bootstrapApplication(AppComponent, {
@@ -44,28 +37,18 @@ export function app(): express.Express {
                 {
                     document: indexHtml,
                     url: req.originalUrl,
-                    // platform‑level providers (e.g. for zone shims) go here:
-                    platformProviders: [],
                 },
             );
-
             res.send(html);
-        } catch (err) {
-            next(err);
+        } catch (e) {
+            next(e);
         }
     });
 
     return server;
 }
 
-function run(): void {
-    const port = process.env['PORT'] || 4000;
-
-    // Start up the Node server
-    const server = app();
-    server.listen(port, () => {
-        console.log(`Node Express server listening on http://localhost:${port}`);
-    });
-}
-
-run();
+const port = process.env['PORT'] || 4000;
+app().listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
